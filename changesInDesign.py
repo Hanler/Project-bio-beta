0fab102fcd182f40d72e5a72657de239ac4778be
Hanler <hmelnitzkij.danil85@gmail.com>
2021-08-03 12:35:38 +0300

    Advanced

--- changesInDesign.py
+++ changesInDesign.py
@@ -2,4 +2,5 @@ from PyQt5 import QtGui
 
 class Changer():
     def change(self):
+        self.pushButton.setStyleSheet("padding: 10px 20px; color: red;")
         self.pushButton_2.setStyleSheet("padding: 10px 20px; color: red;")
